f634f5d7a55707b0ef31f9b21fc89a2a5f6a518e
joe bussard <jbussard@ucsc.edu>
2022-03-09 20:53:28 -0800

    adding documentation

--- src/application.py
+++ src/application.py
@@ -7,16 +7,23 @@ import random
 WORD_LENGTH = 5
 
 class text_colors:
+    """Defines how to use colors for terminals
+    TODO: Does this work on other machines?
+    TODO: Does this work on Windows terminals?"""
     BOLD = '\033[1m'
     YELLOW = BOLD + '' + '\033[93m'
     GREEN = BOLD + '' + '\033[96m'
     RED = BOLD + '' + '\033[91m'
     END = '\033[0m'
 
+# Easier lookup (text_has[x]) than text_colors.x
 text_hash = {'yellow':text_colors.YELLOW,'green':text_colors.GREEN,'red':text_colors.RED,'white':''}
 
-
 def load_dicts():
+    """Opens the 2 dictionaries in use, a smaller dictionary
+    of more common words, and a larger dictionary with
+    words that the user could guess.
+    TODO: Make this an API call to improve space constraints"""
     common_words = []
     all_words = []
     with open('common-fives') as f:
@@ -31,9 +38,11 @@ def load_dicts():
 
 
 def generate_word(length):
+    """legacy"""
     return 'pasta'
 
 def generate_test_cases():
+    """legacy"""
     cases = ['pasta', 'ppppp', 'aaaaa', 'sssss', \
             'ttttt', 'altar', 'whack', 'crack',  \
             'atsap', 'sapta', 'tasta', 'tatat', \
@@ -42,6 +51,9 @@ def generate_test_cases():
     return cases
 
 def check_guess(guess, word):
+    """checks if a guess matches a word. returns a hash map.
+    the hash map is a mapping from i in range(0,6) and 'yellow', 'green', or 'red'"""
+
     # print("----------------------------")
     guess_list = list(guess)
     word_list = list(word)
@@ -73,10 +85,6 @@ def check_guess(guess, word):
             word_index += 1
         index += 1
 
-#     print("guess / word", guess, word)
-#     print("yellow letters", yellow_letters)
-#     print("green letters", green_letters)
-
     # green letters take priority over yellow letters
     # if the same letter is yellow AND green, it gets return as green only.
     result = {}
@@ -87,6 +95,8 @@ def check_guess(guess, word):
     return result
 
 def update_keyboard(key_map, color_map, guess):
+    """Input is a keyboard map. It updates that keyboard map and returns it.
+    The keyboard map connects the keys 'qwerty....bnm' to collors 'red', 'yellow', or 'green'"""
     # updates the keyboard
     guess_list = list(guess)
     for x in color_map:
@@ -96,6 +106,8 @@ def update_keyboard(key_map, color_map, guess):
     return key_map
 
 def create_keyboard_map():
+    """Creates the keyboard map. Initializes everything to "grey" because
+    the user does not know if the letter is in use or not."""
     keys_only = ['q','w','e','r','t','y','u','i','o','p','a','s','d','f','g',
             'h','j','k','l','z','x','c','v','b','n','m']
     key_map = {}
@@ -105,6 +117,7 @@ def create_keyboard_map():
     return key_map
 
 def pretty_print_keyboard(key_map):
+    """prints the keyboard, color-coded to show which letters are in the word."""
     print("")
     for y in key_map:
         if y == 'a':
@@ -124,10 +137,14 @@ def pretty_print_keyboard(key_map):
     print("")
 
 def create_turn_list():
+    """Might get rid of this."""
     turns = [ [] * 5] * 6
     #print(turns)
 
 def update_all(guess, word, key_map, index_map_history):
+    """Run this command after each turn.  It updates the keyboard map,
+    and it updates the index map history. Index map history is what I
+    call the mapping of letters in each guess to their colors."""
     # check_guess returns a map from [0...4] to [green, yellow, etc]
     index_color_map = check_guess(guess, word)
     index_map_history.append(index_color_map)
@@ -136,6 +153,7 @@ def update_all(guess, word, key_map, index_map_history):
     return index_color_map, key_map
 
 def create_emoji_hash():
+    """returns a dict object hashmap that maps color words to emojis"""
     emoji_hash = {}
     emoji_hash['white']  = '⬜'
     emoji_hash['yellow'] = '🟨'
@@ -145,12 +163,15 @@ def create_emoji_hash():
     return emoji_hash
 
 def pretty_print_share_box(index_color_map_history, emoji_hash):
+    """prints the box you get at the end that visualizes guess history"""
     for row in index_color_map_history:
         for column in row:
             print(emoji_hash[row[column]], sep='', end='')
         print("")
 
 def generate_share_box(index_color_map_history, emoji_hash):
+    """creates the box you get at the end that visualizes guess history,
+    but it does not print it, returns a string."""
     my_string = ''
     for row in index_color_map_history:
         for column in row:
@@ -159,14 +180,18 @@ def generate_share_box(index_color_map_history, emoji_hash):
     return my_string
 
 def pretty_print_blank_lines(emoji_hash, color):
+    """directly prints blank lines with the emoji block of a given color.
+    used to visualize the remaining guesses."""
     for x in range(0, 5):
         print("", emoji_hash[color], " ", sep='', end='')
     print(f"\n")
 
 def generate_losing_message(the_answer):
+    """useless"""
     print("You lost! The answer was", the_answer)
 
 def generate_share_text(guesses, index_color_map_history, emoji_hash, game_day):
+    """generates the full share-text that you copy and paste for friends."""
     string = ''
     string += "Polywordle #" + str(game_day)
     guess_num = guesses if guesses < 6 else 'X'
@@ -176,6 +201,7 @@ def generate_share_text(guesses, index_color_map_history, emoji_hash, game_day):
     return string
 
 def pretty_print_index_color(index_color_map, guess, emoji_hash):
+    """prints the letters for a guess in the colors that correspond to its game status."""
     guess_list = list(guess)
     for x in index_color_map:
     #    print('line 122',x)
@@ -183,27 +209,32 @@ def pretty_print_index_color(index_color_map, guess, emoji_hash):
     print(f"\n")
 
 def get_todays_word(common_words):
+    """all it does is pick a random word. this could be replaced with an API."""
     todays_word = random.choice(common_words)
     return todays_word
 
 def test2():
+    """main game logic. initializes several variables, then jumps into the main loop:
+    1. compare a guess
+    2. print the new board
+    3. repeat."""
     guess_history = []
     index_map_history = []
     common_words, all_words = load_dicts()
     game_day = 0
     todays_word = get_todays_word(common_words)
-    print("cheating: todays word is", todays_word)
+    #print("cheating: todays word is", todays_word)
     key_map = create_keyboard_map()
     emoji_hash = create_emoji_hash()
     guesses = 0
     #index_color_map_history = []
 
     print("Welcome to Polywordle [version 0.1]")
+
+    # Print the "blank" board with just empty squares.
     for x in range(guesses, 6):
         pretty_print_blank_lines(emoji_hash, 'white')
-
     pretty_print_keyboard(key_map)
-# TODO - add the grid of guesses
 
     while guesses < 6:
         invalid_guess = True
@@ -243,9 +274,9 @@ def test2():
         elif guesses == 3:
             print("Amazing")
         elif guesses == 4:
-            print("Good job")
+            print("Well done")
         elif guesses == 5:
-            print("Pretty good I guess")
+            print("Pretty good...")
         elif guesses == 6:
             print("That was close")
     else:
@@ -254,14 +285,9 @@ def test2():
     print(f"\nShare your score:\n")
     print(generate_share_text(guesses, index_map_history, emoji_hash, game_day))
 
-    #pretty_print_share_box(index_map_history, emoji_hash)
     exit(0)
 
 test2()
-
-
-
-
 
 def test():
     create_turn_list()
